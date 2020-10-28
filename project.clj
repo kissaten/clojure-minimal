b7707a2c9d89c4aa056ce7be13166cb928b3c284
Manuel Fuchs <malax@malax.de>
2020-10-28 15:44:06 +0100

    Bump clojure, compojure versions

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://clojure-getting-started.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [environ "0.5.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [compojure "1.6.2"]
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [environ "1.2.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
